b9ffccf4e0771f3e37da9912a29f8e5b6055eab1
MiguelFAlvarez <mfa157@hotmail.com>
2015-05-16 23:37:09 +0000

    TCP server operates switches properly

--- TCPServer.py
+++ TCPServer.py
@@ -2,7 +2,8 @@ import socket
 import os
 from threading import Thread
 
-TCP_PORT = 5000
+TCP_IP = '192.168.1.113'
+TCP_PORT = 5015
 BUFFER_SIZE = 1024
 
 onCode = [5313843, 5313987, 5314307, 5315843, 5321987]
@@ -16,10 +17,10 @@ def clientThread(clientSocket, address):
         #Light control data[1] = light command, data[2] = on or off, data[3] = which switch
         if data[1]==1 and data[2]==0:
             print ("Turning on light ", data[3]+1)
-            os.system("/var/www/rfoutlet/codesend ", onCode[data[3]])
+            os.system("/home/pi/PiHome/rfoutlet/codesend %d" % onCode[data[3]])
         if data[1]==1 and data[2]==1:
             print ("Turning off light ", data[3]+1)
-            os.system("/var/www/rfoutlet/codesend ", offCode[data[3]])
+            os.system("/home/pi/PiHome/rfoutlet/codesend %d" % offCode[data[3]])
         print ("received data:", data)
         #clientSocket.send("Turning off light ", data[3]+1)
     clientSocket.close()
@@ -31,11 +32,12 @@ def clientThread(clientSocket, address):
 # create an INET, STREAMing socket
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # bind the socket to a public host, and a well-known port
-serverSocket.bind((socket.gethostname(), 5000))
+serverSocket.bind((TCP_IP, TCP_PORT))
 # become a server socket
 serverSocket.listen(5)
 
 while True:
+    print ("Waiting for Connection")
     # accept connections from outside
     (clientSocket, address) = serverSocket.accept()
     print ('Connection address:',  address)
@@ -44,5 +46,5 @@ while True:
     #ct = clientThread(clientSocket, address)
     ct = Thread(target = clientThread, args = (clientSocket, address))
     ct.start()
-
+serverSocket.close()
 
